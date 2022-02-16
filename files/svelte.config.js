bf603102cccf2b541fb3349d870905205f89f216
robots4life <34193243+robots4life@users.noreply.github.com>
2022-02-16 12:17:01 +0100

    update svelte.config.js

--- files/svelte.config.js
+++ files/svelte.config.js
@@ -1,13 +1,14 @@
+import adapter from '@sveltejs/adapter-auto';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		// target: '#svelte',
-		vite: {
-			server: {
-				host: '0.0.0.0',
-				port: 3000
-			}
+		adapter: adapter()
+	},
+	vite: {
+		server: {
+			host: '0.0.0.0',
+			port: 3000
 		}
 	}
 };
