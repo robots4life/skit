eb02b3ce7a067d864c48f5b5de95e465a8eba643
robots4life <34193243+robots4life@users.noreply.github.com>
2022-04-19 16:18:20 +0200

    remove tailwind classes from elements

--- files/index.svelte
+++ files/index.svelte
@@ -5,5 +5,5 @@
 
 <h1>Welcome to SvelteKit</h1>
 <p>
-	Visit <a class="text-red-400" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
